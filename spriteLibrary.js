cf2f05d7f2ea41fea7485f6e953669628b6653ea
Durvenson <dray@oogzy.com>
2021-01-16 20:55:58 -0500

    Made some code for "sprites"
    cool

---
+++ spriteLibrary.js
@@ -0,0 +1,29 @@
+function Sprite(elementType, relativeCoords, pxCoords, percentCoords, parent, style, inner, size) {
+    let elem = document.createElement(elementType);
+    elem.style = style;
+    elem.style.width = size[0] + "px";
+    elem.style.height = size[1] + "px";
+    elem.style.position = "absolute";
+    elem.style.transform = "translate(" + (relativeCoords[0] - 50) + "%," + (relativeCoords[1] + 50) + "%)";
+    elem.style.left = "calc(" + pxCoords[0] + "px + " + (50 + percentCoords[0]) + "%)";
+    elem.style.bottom = "calc(" + pxCoords[1] + "px + " + (50 + percentCoords[1]) + "%)";
+    elem.innerHTML = inner;
+    parent.append(elem);
+    this.elem = elem;
+    this.getPos = function () {
+        return [relativeCoords, pxCoords, percentCoords];
+    };
+    this.setRelativeCoords = function (pos) {
+        this.elem.style.transform = "translate(" + (pos[0] + 50) + "%," + (pos[1] + 50) + "%)"
+    };
+    this.setPxCoords = function (pos) {
+        this.elem.style.left = "calc(" + pos[0] + "px + " + (50 + percentCoords[0]) + "%)";
+        this.elem.style.bottom = "calc(" + pos[1] + "px + " + (50 + percentCoords[1]) + "%)";
+    };
+    this.setPercentCoords = function (pos) {
+        this.elem.style.left = "calc(" + pxCoords[0] + "px + " + (50 + pos[0]) + "%)";
+        this.elem.style.bottom = "calc(" + pxCoords[1] + "px + " + (50 + pos[1]) + "%)";
+    };
+    this.localVars = {};
+}
+var sprite = new Sprite("button", [0, 0], [0, 0], [0, 0], document.getElementsByTagName("body")[0], "", "", [100, 100]);
